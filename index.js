47f6a878182475ea8e32653525d293166a45b1c0
MeshalS <meshal.saus@gmail.com>
2020-11-09 04:12:03 +0300

    addingthefilessql

---
+++ index.js
@@ -0,0 +1,26 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var util = require("util");
+
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "employee_DB"
+});
+
+connection.query = util.promisify(connection.query);
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+});
